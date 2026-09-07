26a97b9fd6641874fcf3823bb7425c6d016f21a4
kuldeep.singh <kulsin07@gmail.com>
2026-09-07 14:55:21 -0400

    added waits to more steps

--- src/test/java/ShoppingTest.java
+++ src/test/java/ShoppingTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
@@ -16,6 +18,7 @@ public class ShoppingTest
     // We solve that by declaring driver at the class level. Now each object can have its own.
     //The variable exists at the class level, so both setUp() and shoppingTest() can use it.
     WebDriver driver;
+    WebDriverWait wait;
 
     // Imagine you have 20 tests. You don't want usernames and passwords scattered throughout your code.
     // Store test data in variables instead of hardcoding values directly inside Selenium actions.
@@ -27,26 +30,40 @@ public class ShoppingTest
     void setup(){
         // Browser setup will go here
         //Create object + assign reference; now driver can access all methods of Chromedriver class
-        driver = new ChromeDriver(); //
+        driver = new ChromeDriver();
+
+        //constructor call of WebDriverWait class in which we pass arguments
+        //      -Webdriver object,
+        //      - timing in seconds passed to ofSeconds methods in Duration class,
+        //      it's a static method so it's called using class(Duration) name.
+        //  WebdriverWait object created -> Webdriver object passed, Duration object passed-> this info is passed to WebdriverWait object
+        // now we assign the WebDriverWait object to WebDriverWait reference variable
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    @Test
     // @Test tells JUnit that this method is a test case. JUnit Test Runner discovers @Test methods and executes them.
     // No main() method is required because JUnit controls test execution.
-
+    @Test
     public void shoppingTest(){
         driver.get("https://www.saucedemo.com");
         driver.findElement(By.id("user-name")).sendKeys(username);
         driver.findElement(By.id("password")).sendKeys(password);
-        driver.findElement(By.id("login-button")).click();
+
+        //      driver.findElement(By.id("login-button")).click();
+
+        // until is a method of WebDriverWait class
+        // elementToBeClickable is a method of ExpectedConditions class
+        // By is a Selenium class used to create locator objects that tell Selenium how to find an element.
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("login-button"))).click();
+
         driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
-        driver.findElement(By.className("shopping_cart_link")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("shopping_cart_link"))).click();
 
-        String product1= driver.findElement(By.id("item_4_title_link")).getText();
+        String product1= wait.until(ExpectedConditions.elementToBeClickable(By.id("item_4_title_link"))).getText();
         System.out.println(product1);
 
-        String product2= driver.findElement(By.id("item_0_title_link")).getText();
+        String product2= wait.until(ExpectedConditions.elementToBeClickable(By.id("item_0_title_link"))).getText();
         System.out.println(product2);
 
 Assertions.assertEquals("Sauce Labs Backpack", product1);
@@ -56,6 +73,5 @@ Assertions.assertEquals("Sauce Labs Bike Light", product2);
     @AfterEach
     void teardown(){
          driver.quit();
-
     }
 }
